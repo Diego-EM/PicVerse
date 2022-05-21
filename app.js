73a454ff138301a8d05e468f02f13dcbfedb8b86
Diego-EM <dmunozaguilar2000@gmail.com>
2022-05-20 23:46:13 -0500

    img description modal added

--- app.js
+++ app.js
@@ -4,15 +4,20 @@ const API_TOKEN = `P7suvpHZDX31jIYLAXWDrJnbFV9PgTpduapPzn10`;
 const API_URL = `https://api.nasa.gov/planetary/apod?api_key=${API_TOKEN}`;
 
 const backgroundImage = document.getElementById('background_img');
-const backgroundTitle = document.getElementById('img_description');
+const backgroundTitle = document.getElementById('img_title');
+const buttonInfo = document.getElementById('button_info');
+const imgDescription = document.getElementById('img_description');
 
 const setBackground = (date = "") => {
     fetch(API_URL)
         .then(res => res.json())
         .then(data => {
-            console.log(data);
             backgroundImage.src = data.url;
             backgroundTitle.textContent = data.title;
+            document.getElementById('title').textContent = data.title;
+            document.getElementById('author').textContent = data.copyright;
+            document.getElementById('date').textContent = data.date;
+            document.getElementById('description').textContent = data.explanation;
         })
         .catch(e => {
             console.log('Oooops, an error has ocurred: ', e);
@@ -24,8 +29,14 @@ setBackground();
 backgroundImage.onload = function(){
     this.classList.add('presentation');
     backgroundTitle.classList.add('show_title');
+    buttonInfo.classList.remove('invisible');
     this.addEventListener('animationend',()=>{
         this.classList.remove('presentation');
         backgroundTitle.classList.remove('show_title');
     })
 }
+
+buttonInfo.addEventListener('click',()=>{    
+    imgDescription.classList.toggle('hidden');
+    backgroundImage.classList.toggle('blur');
+})
